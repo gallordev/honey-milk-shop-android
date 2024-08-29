bdf4ff204c68095cf30705d734c0f78ef00a5f43
Oswaldo Gallo Romero <91449184+gallordev@users.noreply.github.com>
2024-08-29 16:58:00 -0600

    Initial logic to update/delete campaign

--- app/src/main/java/com/honeymilk/shop/ui/campaign/CampaignListAdapter.kt
+++ app/src/main/java/com/honeymilk/shop/ui/campaign/CampaignListAdapter.kt
@@ -1,7 +1,11 @@
 package com.honeymilk.shop.ui.campaign
 
 import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.annotation.MenuRes
 import androidx.databinding.DataBindingUtil
 import com.honeymilk.shop.R
 import com.honeymilk.shop.databinding.LayoutCampaignItemBinding
@@ -9,7 +13,9 @@ import com.honeymilk.shop.model.Campaign
 import com.honeymilk.shop.ui.common.DataBoundListAdapter
 
 class CampaignListAdapter(
-    private val onCampaignClick: ((Campaign) -> Unit)? = null
+    private val onCampaignClick: ((Campaign) -> Unit)? = null,
+    private val onUpdateCampaignClick: ((Campaign) -> Unit)? = null,
+    private val onDeleteCampaignClick: ((Campaign) -> Unit)? = null
 ) : DataBoundListAdapter<Campaign, LayoutCampaignItemBinding>(
     CampaignDiffCallback()
 ) {
@@ -26,5 +32,23 @@ class CampaignListAdapter(
         binding.campaignCard.setOnClickListener {
             onCampaignClick?.invoke(item)
         }
+        binding.btnMore.setOnClickListener {
+            showMenu(it, R.menu.menu_popup_campaign)
+        }
+    }
+
+    private fun showMenu(v: View, @MenuRes menuRes: Int) {
+        val popup = PopupMenu(v.context, v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            // Respond to menu item click.
+            true
+        }
+        popup.setOnDismissListener {
+            // Respond to popup being dismissed.
+        }
+        // Show the popup menu.
+        popup.show()
     }
 }
--- app/src/main/java/com/honeymilk/shop/ui/campaign/CampaignListFragment.kt
+++ app/src/main/java/com/honeymilk/shop/ui/campaign/CampaignListFragment.kt
@@ -22,11 +22,28 @@ class CampaignListFragment :
     private val campaignListViewModel: CampaignListViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupMenu()
+        adapter = CampaignListAdapter(
+            onCampaignClick = {
+                findNavController().navigate(
+                    CampaignListFragmentDirections.actionCampaignListFragmentToCampaignDetailFragment(
+                        it.id
+                    )
+                )
+            }
+        )
+        binding.recyclerViewCampaignList.adapter = adapter
+        campaignListViewModel.campaigns.observe(viewLifecycleOwner) {
+            val campaignList = it ?: return@observe
+            adapter.submitList(campaignList)
+        }
+    }
+
+    private fun setupMenu() {
         val menuProvider = object : MenuProvider {
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                 menuInflater.inflate(R.menu.menu_campaign_list, menu)
             }
-
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return when (menuItem.itemId) {
                     R.id.new_campaign -> {
@@ -38,21 +55,8 @@ class CampaignListFragment :
                     else -> false
                 }
             }
-
         }
         requireActivity().addMenuProvider(menuProvider, viewLifecycleOwner, Lifecycle.State.RESUMED)
-        adapter = CampaignListAdapter {
-            findNavController().navigate(
-                CampaignListFragmentDirections.actionCampaignListFragmentToCampaignDetailFragment(
-                    it.id
-                )
-            )
-        }
-        binding.recyclerViewCampaignList.adapter = adapter
-        campaignListViewModel.campaigns.observe(viewLifecycleOwner) {
-            val campaignList = it ?: return@observe
-            adapter.submitList(campaignList)
-        }
     }
 
 }
