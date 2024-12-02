555979f843cc7d32a4dd3a0ab31eb90901d5a248
Oswaldo Gallo Romero <gallor.dev@gmail.com>
2024-12-02 13:13:59 -0600

    temp

--- app/src/main/java/com/honeymilk/shop/ui/order/NewOrderItemDialogFragment.kt
+++ app/src/main/java/com/honeymilk/shop/ui/order/NewOrderItemDialogFragment.kt
@@ -1,7 +1,6 @@
-package com.honeymilk.shop.ui.design
+package com.honeymilk.shop.ui.order
 
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
@@ -13,54 +12,57 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputLayout
 import com.honeymilk.shop.R
 import com.honeymilk.shop.model.Design
+import com.honeymilk.shop.ui.design.DesignListViewModel
+import com.honeymilk.shop.ui.design.DesignsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DesignListDialogFragment(
-    private val callBack: ((List<Design>) -> Unit)? = null
-): DialogFragment() {
+class NewOrderItemDialogFragment : DialogFragment() {
+
     private lateinit var adapter: DesignsAdapter
     private val viewModel: DesignListViewModel by viewModels()
-    private lateinit var selectedDesigns: List<Design>
+    private var currentStep = 1
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        selectedDesigns = emptyList()
         val builder = MaterialAlertDialogBuilder(requireActivity())
         val inflater = requireActivity().layoutInflater
         val view = inflater.inflate(R.layout.fragment_design_list, null)
         builder.setTitle(getString(R.string.title_select_order_products))
         builder.setView(view)
-        val tempSelectedDesigns = mutableListOf<Design>()
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-        adapter = DesignsAdapter(clickCallback = { d: Design, v: MaterialCardView ->
-            v.isChecked = !v.isChecked
-            if (v.isChecked) {
-                tempSelectedDesigns.add(d)
-            } else {
-                tempSelectedDesigns.remove(d)
-            }
+            .setPositiveButton(getString(R.string.btn_next)) { _, _ -> handleNextStep() }
+            .setNegativeButton(getString(R.string.btn_cancel)) { dialog, _ -> dialog.dismiss() }
+        adapter = DesignsAdapter(clickCallback = { d: Design, _: MaterialCardView ->
+
         })
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = adapter
+        view.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = this@NewOrderItemDialogFragment.adapter
+        }
         viewModel.filteredDesigns.observe(requireActivity()) {
             val designs = it ?: return@observe
             adapter.submitList(designs)
         }
-
         val searchField = view.findViewById<TextInputLayout>(R.id.textField_search)
         searchField.editText?.addTextChangedListener {
             viewModel.searchQuery.value = it.toString()
         }
 
-        builder.setPositiveButton(getString(R.string.btn_add)) { _, _ ->
-            selectedDesigns = tempSelectedDesigns
-            callBack?.invoke(selectedDesigns)
-        }
-        builder.setNegativeButton(getString(R.string.btn_cancel)) { dialog: DialogInterface, _: Int ->
-            selectedDesigns = emptyList()
-            dialog.dismiss()
-        }
         return builder.create()
     }
+
+    private fun handleNextStep() {
+//        when (currentStep) {
+//            1 -> {
+//                currentStep++
+//                updateContent(R.layout.dialog_wizard_step2)
+//            }
+//            2 -> {
+//                currentStep++
+//                updateContent(R.layout.dialog_wizard_step3)
+//            }
+//            else -> dismiss()
+//        }
+    }
+
 
 }
